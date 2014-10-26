8be9407e224e69c017f3708502a884bbb67ab0ce
old21nick21 <henrypush@msn.com>
2014-10-26 13:14:53 -0400

    Teacher's example, commented
    Added comments to the original example, provided by the course staff.

--- cachematrix.R
+++ cachematrix.R
@@ -1,41 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create a vector using cached mean value, if available
+makeVector <- function(x = numeric()) {
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-  ##
+  ## Init  value for m
   m <- NULL
+  ## set values, assigning them from the other environmnet
+  ## evoking cachemean function in process
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
+  ## returns x
   get <- function() x
+  ## sets the value of mean using the other environment
   setmean <- function(mean) m <<- mean
+  ## returns value of the mean, if any
   getmean <- function() m
+  ## finally generate a vector with all available values
   list(set = set, get = get,
        setmean = setmean,
        getmean = getmean)
-  ##
+
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  ##
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
+## Checks if mean is already calculated, 
+## and perfroms the calculation, if mean value is empty
+cachemean <- function(x, ...) {
+  ## checks getmean values and uses them if available
+  ## giving user a warning message
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)  ## exits the function
   }
-  get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
-  list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
-  ##
+  ## finds out the value of x
+  data <- x$get()
+  ## calculates the mean
+  m <- mean(data, ...)
+  ## sets the mean value in the other environment 
+  x$setmean(m)
+  ## returns m (mean value)
+  m
 }
